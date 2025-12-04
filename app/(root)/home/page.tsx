6a1e96bc70605dfc0f12727022a0878fcc905540
SantinoGiordano <santinogiordano13@gmail.com>
2025-12-04 15:25:44 -0500

    mapped data

--- app/(root)/home/page.tsx
+++ app/(root)/home/page.tsx
@@ -1,7 +1,32 @@
+"use client";
+
+import { Movie } from "@/types/page";
+import { use, useEffect, useState } from "react";
+
 export default function Home() {
-    return (
-        <div>
-            Home Page
-        </div>
-    )
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    // Fetch movies from an API or database
+    fetch("/api/movies")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
+
+  return(
+    <div>
+        {movies.map((movie: Movie) => (
+            <>
+            <div>
+            <div key={movie.id}>
+                <h2>{movie.title}</h2>  
+                <p>{movie.description}</p>
+            </div>
+            </div>
+            </>
+        ))}
+    </div>
+
+  )
+    
 }
