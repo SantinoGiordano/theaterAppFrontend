ff093f0bd356a4c52f2d5aac7bc136808d043465
SantinoGiordano <santinogiordano13@gmail.com>
2025-12-09 15:19:13 -0500

    changed css

--- app/(root)/home/page.tsx
+++ app/(root)/home/page.tsx
@@ -84,7 +84,7 @@ export default function Home() {
               {selectedMovie.showtimes.map((time, index) => (
                 <div
                   key={index}
-                  className="px-4 py-2 bg-purple-100 text-purple-800 rounded-full text-center font-medium shadow-sm"
+                  className="px-4 py-2 bg-blue-800 text-blue-100 rounded-full text-center font-medium shadow-sm"
                 >
                   {time}
                 </div>
