a18f1418c9059c8e106238b4980c6c8f63ff4eea
SantinoGiordano <santinogiordano13@gmail.com>
2025-12-10 13:53:03 -0500

    created a layout for each section of the website, root main and auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
-
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/navbar";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,16 +20,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Navbar />
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} bg-white`}>
+        <main className="p-6">{children}</main>
       </body>
     </html>
   );
