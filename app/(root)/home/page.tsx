1750fa132b05ebd7fd96f6e48e925e421123fff2
SantinoGiordano <santinogiordano13@gmail.com>
2025-12-10 14:06:37 -0500

    placholder text

---
+++ app/(root)/home/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <>
+        <div>
+            <h1 className="text-4xl font-bold text-center mt-10">Welcome to the Movie Theater App</h1>
+        </div>
+        </>
+    )
+}
