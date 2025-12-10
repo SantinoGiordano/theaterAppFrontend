4480fa952f81bd7459db533a73d3f5b194ffbfda
SantinoGiordano <santinogiordano13@gmail.com>
2025-12-10 09:55:17 -0500

    added food item / food size interfaces

--- types/page.tsx
+++ types/page.tsx
@@ -1,12 +1,29 @@
 export interface Movie {
-    id: string;
-    name: string;
-    description: string;
-    runtime: number;
-    releaseDate: string;
-    rating: number;
-    genre: string[];
-    showtimes: string[];
-    directors: string;
-    img?: string;
+  id: string;
+  name: string;
+  description: string;
+  runtime: number;
+  releaseDate: string;
+  rating: number;
+  genre: string[];
+  showtimes: string[];
+  directors: string;
+  img?: string;
 }
+
+export interface FoodSize {
+  size: "S" | "M" | "L";
+  price: number;
+  calories: number;
+}
+
+export interface FoodItem {
+  _id: string;
+  name: string;
+  category: string;
+  img: string;
+  sizes?: FoodSize[];
+  price?: number;
+  calories?: number;
+}
+
