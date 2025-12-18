fecbf25c28e62e2a2bae4cfdc735012310928fbd
SantinoGiordano <santinogiordano13@gmail.com>
2025-12-18 14:40:49 -0500

    moved header into the body, for background coloring

--- app/(root)/comingSoon/page.tsx
+++ app/(root)/comingSoon/page.tsx
@@ -41,15 +41,6 @@ export default function ComingSoon() {
 
   return (
     <>
-      <div className="text-center mt-6">
-        <h2 className="text-2xl text-red-500 font-bold">
-          Coming Soon to Theaters
-        </h2>
-        <p className="text-red-400 mt-2">
-          Stay tuned for exciting upcoming releases!
-        </p>
-      </div>
-
       <div className="carousel w-full">
         {movies.map((movie, index) => {
           const slideId = index + 1;
@@ -60,18 +51,34 @@ export default function ComingSoon() {
             <div
               key={movie._id}
               id={`slide${slideId}`}
-              className="carousel-item relative w-full"
+              className="carousel-item relative w-full bg-gray-900 bg-opacity-90 rounded-lg shadow-lg border border-red-700"
             >
-              <div className="w-full flex justify-center items-center">
+              <div className="w-full flex flex-col items-center justify-center p-6">
+                <div className="text-center mt-6">
+                  <h2 className="text-2xl text-red-500 font-bold">
+                    Coming Soon to Theaters
+                  </h2>
+                  <p className="text-red-400 mt-2">
+                    Stay tuned for exciting upcoming releases!
+                  </p>
+                </div>
                 <Image
+                draggable={false}
                   src={`/comingSoonMoviePosters/${movie.img}`}
                   alt={movie.title ?? "Coming Soon Movie Poster"}
                   width={500}
                   height={500}
-                  className="pt-10 w-full max-w-[500px] h-auto object-contain mx-auto"
+                  className="pt-10 w-full max-w-[500px] h-auto object-contain mx-auto drop-shadow-lg"
                 />
+                {movie.description && (
+                  <p className="mt-6 text-center text-lg max-w-xl text-gray-200 bg-gradient-to-r from-black via-gray-900 to-black rounded px-4 py-3 border-t border-b border-red-700 shadow-md">
+                    <span className="text-red-500 font-semibold">
+                      Description:{" "}
+                    </span>
+                    <span className="text-gray-200">{movie.description}</span>
+                  </p>
+                )}
               </div>
-
               <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                 <a href={`#slide${prevSlide}`} className="btn btn-circle">
                   ❮
