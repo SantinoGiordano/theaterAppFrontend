8ca05c9fbcb1d5d121f403f91e55ae73f5fbcd07
SantinoGiordano <santinogiordano13@gmail.com>
2026-01-08 14:39:17 -0500

    created responsive shaping in the movies stage

--- app/(root)/home/page.tsx
+++ app/(root)/home/page.tsx
@@ -100,24 +100,37 @@ export default function Home() {
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 px-6 max-w-6xl mx-auto">
           {movies.map((movie: Movie, index) => (
             <div
-              onClick={() => {
-                router.push("./movies");
-              }}
-              key={index}
-              className="bg-gray-900 p-4 rounded-lg shadow-lg hover:scale-105 border transition"
-            >
-              <img
-                draggable="false"
-                alt={movie.name}
-                src={`moviePosters/${movie.img}`}
-                className="rounded-lg w-full"
-              />
-              <h3 className="text-xl font-semibold mt-4">{movie.name}</h3>
-              <hr className="my-2 border-white" />
-              <div className="text-sm text-gray-400  mt-4">
-                {movie.description}
-              </div>
-            </div>
+  onClick={() => router.push("./movies")}
+  key={index}
+  className="
+    bg-gray-900 p-4 rounded-lg shadow-lg border
+    transition
+    cursor-pointer
+    md:hover:scale-105
+  "
+>
+  <img
+    draggable="false"
+    alt={movie.name}
+    src={`moviePosters/${movie.img}`}
+    className="
+      rounded-lg w-full
+      aspect-[2/3]
+      object-cover
+    "
+  />
+
+  <h3 className="text-lg sm:text-xl font-semibold mt-4">
+    {movie.name}
+  </h3>
+
+  <hr className="my-2 border-white/30" />
+
+  <div className="text-sm text-gray-400 line-clamp-3">
+    {movie.description}
+  </div>
+</div>
+
           ))}
         </div>
       </section>
